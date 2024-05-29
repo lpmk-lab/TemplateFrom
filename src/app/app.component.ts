8c5192e4b46849d4ea89701d3878aa6fc6820423
lapyaeminkhant.sbs <lapyaeminkhant.sbs@gmail.com>
2024-05-29 21:00:48 +0630

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TemplateFrom';
+ @ViewChild('registFrom') from:NgForm
+  Onsubimt( ){
+    console.log(this.from);
+
+  }
 }
